6f3691a42a7c350a0b990a9247afe72d9f5c0a91
martuseri <martiinaseri@gmail.com>
2026-09-25 19:32:04 -0300

    Add files via upload

---
+++ for.php
@@ -0,0 +1,30 @@
+<?php
+
+//Ejemplo 1: Incremento:
+for($a=1; $a<=20; $a++){
+  echo $a."<br>";
+}
+
+echo "<br>";
+
+//Ejemplo 1: Decremento:
+for($a=20; $a>=1; $a--){
+  echo $a."<br>";
+}
+
+echo "<br>";
+
+//Ejemplo 2: Incremento:
+$numero=7;
+for($a=1; $a<=12; $a++){
+  echo $numero." X ".$a." = ".$a*$numero."<br>";
+}
+
+echo "<br>";
+
+
+//Ejemplo 2: Incremento:
+$numero=7;
+for($a=12; $a>=1; $a--){
+  echo $numero." X ".$a." = ".$a*$numero."<br>";
+}
